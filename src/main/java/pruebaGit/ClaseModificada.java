8aad7b71fbd00587223efc1634cb027193ad76c5
dani <daniel.lara.salduba@gmail.com>
2020-06-02 14:25:04 +0200

    Añadida la clase ClaseModificada

---
+++ src/main/java/pruebaGit/ClaseModificada.java
@@ -0,0 +1,23 @@
+
+package pruebaGit;
+
+
+public class ClaseModificada {
+    
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "ClaseModificada{" + "nombre=" + nombre + '}';
+    }
+    
+    
+}
