aef166fcb2b2e00d5cfefc4a3e7aab65397988ae
dani <daniel.lara.salduba@gmail.com>
2020-06-02 14:01:28 +0200

    Añadida la clase UsarPruebaGit

---
+++ src/main/java/pruebaGit/UsarPruebaGit.java
@@ -0,0 +1,18 @@
+
+package pruebaGit;
+
+
+public class UsarPruebaGit {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo desde Git");
+        
+        PruebaGit prueba1 = new PruebaGit();
+        prueba1.numero = 20;
+        
+        System.out.println(prueba1.numero);
+    }
+    
+}
